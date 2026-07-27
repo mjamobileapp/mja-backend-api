3c9eedc5609beed1cebd6209c85ea5f4d2d11a2e
Hamdan <Hamdanfresh@gmail.com>
2026-07-28 00:05:29 +0700

    feat: allow cashier to read master items

--- src/routes/masterItem.js
+++ src/routes/masterItem.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const MasterItemController = require("../controller/masterItem");
 const { authenticate } = require("../middleware/auth");
+const { authenticateBackofficeOrOwnerKasir } = require("../middleware/authCombined");
 const { catchAsync } = require("../utils/catchAsync");
 
 const router = express.Router();
 
 // Daftarkan rute POST untuk master item expense
 router.post("/", authenticate, catchAsync(MasterItemController.createNewMasterItem));
-router.get("/", authenticate, catchAsync(MasterItemController.getAllMasterItem));
-router.get("/:id", authenticate, catchAsync(MasterItemController.getMasterItemById));
-router.get("/tipe/:tipeItem", authenticate, catchAsync(MasterItemController.getMasterItemByTipe));
+router.get("/", authenticateBackofficeOrOwnerKasir(), catchAsync(MasterItemController.getAllMasterItem));
+router.get("/tipe/:tipeItem", authenticateBackofficeOrOwnerKasir(), catchAsync(MasterItemController.getMasterItemByTipe));
+router.get("/:id", authenticateBackofficeOrOwnerKasir(), catchAsync(MasterItemController.getMasterItemById));
 router.put("/:id", authenticate, catchAsync(MasterItemController.updateMasterItem));
 router.delete("/:id", authenticate, catchAsync(MasterItemController.deleteMasterItem));
 router.post("/:id/restore", authenticate, catchAsync(MasterItemController.restoreMasterItem));
