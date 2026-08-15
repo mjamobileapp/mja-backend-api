1226ccdaab49e1913c18c7dcc20697856626acfa
Hamdan <Hamdanfresh@gmail.com>
2026-08-15 21:48:47 +0700

    Add route API getHistoryMesinBackoffice

---
+++ src/routes/historyBackoffice.js
@@ -0,0 +1,11 @@
+﻿const express = require("express");
+const HistoryController = require("../controller/history");
+const { authenticate } = require("../middleware/auth");
+const { catchAsync } = require("../utils/catchAsync");
+
+const router = express.Router();
+
+// GET - Get History Log Eksekusi Mesin (Backoffice)
+router.get("/mesin", authenticate, catchAsync(HistoryController.getHistoryMesinBackoffice));
+
+module.exports = router;
